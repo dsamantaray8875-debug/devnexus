3e03c4c2add70d0f3727d122725eae53e789b161
root <root@ip-172-31-13-120.us-west-1.compute.internal>
2026-09-14 08:43:43 +0000

    feat: create DevNexus VPC networking

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -2,10 +2,46 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_cidr = "10.20.0.0/16"
+
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+
+  public_subnet_cidrs = [
+    "10.20.1.0/24",
+    "10.20.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.20.11.0/24",
+    "10.20.12.0/24"
+  ]
+}
+
 output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 
 output "aws_region" {
   value = data.aws_region.current.region
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
 }
